384b219e5680d844393232439142f812ac9894b8
Me <migjig.20@gmail.com>
2017-07-24 07:07:03 -0400

    Couse Version: Section 3 - lesson 27-31

---
+++ Section2/Assets/Scripts/ShowStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShowStats : MonoBehaviour {
+
+	private Rigidbody body;
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(body.inertiaTensor);
+		Debug.Log(body.centerOfMass);
+	}
+}
